6fa7c01853fe44af1318e85c4995a8789c7903d7
Tony Chen <tony_chen@navyfederal.org>
2022-11-03 16:18:15 -0400

    JS Functionality added

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ const username = document.getElementsByClassName("username-input")[0];
 const passy = document.getElementsByClassName("password-input")[0];
 const error = document.getElementsByClassName("error-message-div")[0];
 
-if (!passy.value.match(/[0-9]/)) {
+if (!passy.value.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/) || passy.value.length < 6) {
 error.textContent = "Password must be more than 6 digits and contain at least one number, one uppercase letter, and one lowercase letter";
 error.style.color = "red";
-} else if (!passy.value.match(/[A-Z]/)) {
-error.textContent = "Password must be more than 6 digits and contain at least one number, one uppercase letter, and one lowercase letter";
-error.style.color = "red";
-} else if (!passy.value.match(/[a-z]/)) {
-error.textContent = "Password must be more than 6 digits and contain at least one number, one uppercase letter, and one lowercase letter";
-error.style.color = "red";
-} else if (passy.value.length < 6) {
-error.textContent = "Password must be more than 6 digits and contain at least one number, one uppercase letter, and one lowercase letter";
-error.style.color = "red";
-} else {
+}
+else {
 error.textContent = ""
 }
 }
